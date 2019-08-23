38ee268c7f3bfcf91545ae23747c0de3e7baad70
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-08-23 13:44:14 +0200

    added bench link

--- esm/index.js
+++ esm/index.js
@@ -8,3 +8,13 @@ export default function (template) {
     args.push(arguments[i++]);
   return args;
 };
+
+/**
+ * best benchmark goes here
+ * https://jsperf.com/tta-bench
+ * I should probably have an @ungap/template-literal-es too
+export default (...args) => {
+  args[0] = unique(args[0]);
+  return args;
+};
+ */
